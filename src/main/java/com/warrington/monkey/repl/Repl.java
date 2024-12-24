b27bc69b13de6aede0db99a80f6ccea58741b6c4
Alex Warrington <awarrington0895@gmail.com>
2024-12-24 10:09:01 -0600

    feat(repl): Enable evaluator REPL
    pg 114

--- src/main/java/com/warrington/monkey/repl/Repl.java
+++ src/main/java/com/warrington/monkey/repl/Repl.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.warrington.monkey.ast.Program;
+import com.warrington.monkey.evaluator.Evaluator;
 import com.warrington.monkey.lexer.Lexer;
+import com.warrington.monkey.object.MonkeyObject;
 import com.warrington.monkey.parser.Parser;
 import com.warrington.monkey.token.Token;
 import com.warrington.monkey.token.TokenType;
@@ -13,6 +15,36 @@ public class Repl {
     private static final String PROMPT = ">> ";
 
     private Repl() {
+    }
+
+    public static void start() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print(PROMPT);
+
+                final String line = scanner.nextLine();
+
+                if (line == null) {
+                    return;
+                }
+
+                final var lexer = new Lexer(line);
+                final var parser = new Parser(lexer);
+
+                final Program program = parser.parseProgram();
+
+                if (!parser.errors().isEmpty()) {
+                    printParserErrors(parser.errors());
+                    continue;
+                }
+
+                final MonkeyObject evaluated = Evaluator.eval(program);
+
+                if (evaluated != null) {
+                    System.out.println(evaluated.inspect());
+                }
+            }
+        }
     }
 
     public static void startParser() {
