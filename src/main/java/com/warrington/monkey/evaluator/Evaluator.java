663d7ce23077e049cb7fec142a1f36218b6ba23c
Alex Warrington <awarrington0895@gmail.com>
2024-12-24 10:17:42 -0600

    feat(eval): Add constant for Null

--- src/main/java/com/warrington/monkey/evaluator/Evaluator.java
+++ src/main/java/com/warrington/monkey/evaluator/Evaluator.java
@@ -4,13 +4,17 @@ import com.warrington.monkey.ast.*;
 import com.warrington.monkey.object.Bool;
 import com.warrington.monkey.object.Int;
 import com.warrington.monkey.object.MonkeyObject;
+import com.warrington.monkey.object.Null;
 
 import java.util.List;
 
 public class Evaluator {
 
+    // No need to allocate new objects for true/false whenever it is encountered
+    // Can simply reference these constants
     private static final Bool TRUE = new Bool(true);
     private static final Bool FALSE = new Bool(false);
+    private static final Null NULL = new Null();
 
     public static MonkeyObject eval(Node node) {
         return switch (node) {
